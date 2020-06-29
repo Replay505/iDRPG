82758fe47decb0bf8d661be69aa3d6ac5d023104
Replay505 <40672925+Replay505@users.noreply.github.com>
2020-06-29 12:25:20 -0700

    Tile-based movement Complete

--- iDRPG/Assets/Scripts/PlayerMovement.cs
+++ iDRPG/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = .1f;
+    public int moveCount = 4;
+    public string direction;
+
+    public Animator playerAnim;
+    public SpriteRenderer playerRend;
     public Transform movePoint;
+
+    public LayerMask colliders;
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
+        playerAnim = GetComponentInChildren<Animator>();
+        playerRend = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizonal") == )
-        { 
-        
+        //Makes player sprite always move towards the target point, which is moved below vvv
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .02f)
+        {
+            //These four ifs are for animation
+            if (Input.GetAxisRaw("Horizontal") == 1)
+            {
+                playerAnim.Play("Side");
+                playerRend.flipX = true;
+                direction = "Right";
+            }
+            if (Input.GetAxisRaw("Horizontal") == -1)
+            {
+                playerAnim.Play("Side");
+                playerRend.flipX = false;
+                direction = "Left";
+            }
+            if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                playerAnim.Play("Backward");
+                direction = "Up";
+            }
+            if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                playerAnim.Play("Forward");
+                direction = "Down";
+            }
+
+            //For Horizontal Movement (A/D)
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1 && moveCount != 0)
+            {
+                //This if is for checking the tile infront of the direction and moving there
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, colliders))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    moveCount--;
+                }
+            }
+            //For Vertical Movement (W/S)
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1 && moveCount != 0)
+            {
+                //This if is for checking the tile infront of the direction and moving there
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, colliders))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    moveCount--;
+                }
+            }
         }
     }
 }

dfc9c297be9f504c9c58294baf145da8427fb639
Replay505 <40672925+Replay505@users.noreply.github.com>
2020-06-29 11:11:47 -0700

    Day 1 Push

---
+++ iDRPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizonal") == )
+        { 
+        
+        }
+    }
+}
